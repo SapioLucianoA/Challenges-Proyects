ead5f8a1359f5c03200ee736129905a683fa8bec
Sapio Luciano <sapioldev@gmail.com>
2025-01-09 17:57:17 -0300

    completed

--- 003-Type-game/js/apiService.js
+++ 003-Type-game/js/apiService.js
@@ -1,6 +1,6 @@
 export async function fetchWords() {
   try {
-    const response = await fetch('https://random-word-api.herokuapp.com/word?number=60');
+    const response = await fetch('https://random-word.ryanrk.com/api/en/word/random/60');
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
